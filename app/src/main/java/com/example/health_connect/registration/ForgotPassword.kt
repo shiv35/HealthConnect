649b460cc397e71fcf4e8ba817a9dfdbd5f97d14
shreyash <shreyashdelmade4@gmail.com>
2024-02-09 23:50:26 +0530

    regis problem

---
+++ app/src/main/java/com/example/health_connect/registration/ForgotPassword.kt
@@ -0,0 +1,12 @@
+package com.example.health_connect.registration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.health_connect.R
+
+class ForgotPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+    }
+}
