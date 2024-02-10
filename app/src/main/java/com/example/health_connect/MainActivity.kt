173308f789a621a3cd6763b5f299b459224006a9
shreyash <shreyashdelmade4@gmail.com>
2024-02-10 15:06:26 +0530

    bottom navigation and drawer , fragments

--- app/src/main/java/com/example/health_connect/MainActivity.kt
+++ app/src/main/java/com/example/health_connect/MainActivity.kt
@@ -1,11 +1,86 @@
 package com.example.health_connect
 
+import android.content.ClipData.Item
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.health_connect.databinding.ActivityMainBinding
+import com.example.health_connect.fragments.ChatFragment
+import com.example.health_connect.fragments.HomeFragment
+import com.example.health_connect.fragments.PrescriptionFragment
+import com.example.health_connect.fragments.TeamFragment
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var fragmentManager : FragmentManager
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+// for navigation drawer
+        setSupportActionBar(binding.toolbar)
+        val toggle = ActionBarDrawerToggle(this,binding.drawerLayout,binding.toolbar,R.string.app_name,R.string.email)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        binding.navigationDrawer.setNavigationItemSelectedListener(this)
+
+        // for bottom navigation
+        binding.bottomNavigation.background = null
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.bottom_home -> openFragment(HomeFragment())
+                R.id.bottom_chat -> openFragment(ChatFragment())
+                R.id.bottom_prescription -> openFragment(PrescriptionFragment())
+            }
+             true
+        }
+        // first default fragment
+        fragmentManager = supportFragmentManager
+        openFragment(HomeFragment())
+
+        binding.fab.setOnClickListener{
+            Toast.makeText(this, "action button", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_chat -> openFragment(ChatFragment())
+            R.id.nav_home -> openFragment(HomeFragment())
+            R.id.nav_team -> openFragment(TeamFragment())
+            R.id.nav_prescription -> Toast.makeText(this, "prescription", Toast.LENGTH_SHORT).show()
+
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed(){
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        else{
+         super.onBackPressed()
+        }
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
+
+    }
+
+
 }
+
---
+++ app/src/main/java/com/example/health_connect/fragments/PrescriptionFragment.kt
@@ -0,0 +1,22 @@
+package com.example.health_connect.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.health_connect.R
+
+
+class PrescriptionFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_prescription, container, false)
+    }
+
+
+}
